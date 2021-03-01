3c4ea38ca87f57430054f4170e5a9d1449c5a465
Santiago Buitrago <brayan.buitrago@mail.escuelaing.edu.co>
2021-02-28 21:32:18 -0500

    Laboratorio 4

--- Parte Laboratorio/src/main/java/edu/eci/arsw/blueprints/services/BlueprintsServices.java
+++ Parte Laboratorio/src/main/java/edu/eci/arsw/blueprints/services/BlueprintsServices.java
@@ -1,19 +1,24 @@
 package edu.eci.arsw.blueprints.services;
 
 import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.filters.BlueprintsFilter;
 import edu.eci.arsw.blueprints.model.Point;
 import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
 import edu.eci.arsw.blueprints.persistence.BlueprintsPersistence;
+import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 
 /**
  * ---------------------------------------------------------------------------------------------------------------------------
  * ---------------------------------------------------------------------------------------------------------------------------
- * 													CLASE: 
+ * 													CLASE: BlueprintsServices
  * ---------------------------------------------------------------------------------------------------------------------------
  * 
  * ---------------------------------------------------------------------------------------------------------------------------
@@ -32,14 +37,19 @@ import org.springframework.stereotype.Service;
 public class BlueprintsServices {
    
     @Autowired
+	@Qualifier("InMemoryBlueprintPersistence")
     BlueprintsPersistence bpp=null;
+	
+	@Autowired
+    @Qualifier("RedundancyFilter")
+    BlueprintsFilter bpf;
     
-    public void addNewBlueprint(Blueprint bp){
-        
+    public void addNewBlueprint(Blueprint bp) throws BlueprintPersistenceException{
+        bpp.saveBlueprint(bp);
     }
     
-    public Set<Blueprint> getAllBlueprints(){
-        return null;
+    public Set<Blueprint> getAllBlueprints() throws BlueprintNotFoundException{
+        return bpp.getAllBlueprints();
     }
     
     /**
@@ -50,7 +60,7 @@ public class BlueprintsServices {
      * @throws BlueprintNotFoundException if there is no such blueprint
      */
     public Blueprint getBlueprint(String author,String name) throws BlueprintNotFoundException{
-        throw new UnsupportedOperationException("Not supported yet."); 
+        return bpp.getBlueprint(author,name);
     }
     
     /**
@@ -60,7 +70,11 @@ public class BlueprintsServices {
      * @throws BlueprintNotFoundException if the given author doesn't exist
      */
     public Set<Blueprint> getBlueprintsByAuthor(String author) throws BlueprintNotFoundException{
-        throw new UnsupportedOperationException("Not supported yet."); 
+        return bpp.getBlueprintsByAuthor(author);
+    }
+	
+	public Blueprint filter(Blueprint blueprint){
+        return bpf.filter(blueprint);
     }
     
 }
